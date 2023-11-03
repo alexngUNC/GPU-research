5d08bb436a21ccfad86ae1001af5f113c46d0d19
Alex Georgiev <ageorgiev@unc.edu>
2023-11-03 11:11:43 -0400

    Add no memory benchmark

---
+++ Preemptions/Benchmarks/xavierNoMemory.cu
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "testbench.h"
+
+__global__ void spin() {
+  while (1) {
+  }
+}
+
+int main() {
+  cudaError_t err;
+  spin<<<16, 1024>>>();
+  SAFE(cudaDeviceSynchronize());
+  printf("Exited\n");
+  return 0;
+}
